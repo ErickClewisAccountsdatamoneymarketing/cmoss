1d16df42e7a81563b8c29682a529a3ba082342aa
Zheng, Lei <realthunder.dev@gmail.com>
2015-09-28 16:55:40 +0800

    Update ldns to 1.6.17, added ldns_buffer patch

--- build-droid/build-libldns.sh
+++ build-droid/build-libldns.sh
@@ -2,7 +2,7 @@
 set -e
 
 PKG_NAME=ldns
-PKG_VERSION=1.6.16
+PKG_VERSION=1.6.17
 PKG_URL=http://www.nlnetlabs.nl/downloads/ldns
 
 . `dirname $0`/common.sh
@@ -14,7 +14,7 @@ cd $PKG_DIR
 LDFLAGS="-Os -pipe -isysroot ${SYSROOT}"
 
 #call_configure --host=${ARCH}-android-linux --target=${PLATFORM} --prefix=${SYSROOT}/usr --disable-gost 
-call_configure --host=${ARCH}-android-linux --target=${PLATFORM} --prefix=${SYSROOT}/usr --disable-gost --disable-ecdsa --without-ssl --disable-sha2 
+call_configure --host=${ARCH}-android-linux --target=${PLATFORM} --prefix=${SYSROOT}/usr --disable-gost --disable-dane --disable-ecdsa --without-ssl --disable-sha2 
 
 make
 make install
