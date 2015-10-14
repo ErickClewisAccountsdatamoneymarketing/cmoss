39a799d3a67e03e6bafacd38854e6e6ec5a9e397
Zheng, Lei <realthunder.dev@gmail.com>
2015-10-15 00:31:34 +0800

    Added missing call to patch ldns

--- build-droid/build-libldns.sh
+++ build-droid/build-libldns.sh
@@ -16,5 +16,7 @@ LDFLAGS="-Os -pipe -isysroot ${SYSROOT}"
 #call_configure --host=${ARCH}-android-linux --target=${PLATFORM} --prefix=${SYSROOT}/usr --disable-gost 
 call_configure --host=${ARCH}-android-linux --target=${PLATFORM} --prefix=${SYSROOT}/usr --disable-gost --disable-dane --disable-ecdsa --without-ssl --disable-sha2 
 
+${TOPDIR}/helper/patch.sh $PKG_NAME -v $PKG_VERSION || exit 1
+
 make
 make install
