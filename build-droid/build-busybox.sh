00c5e2369e4ef7280b79af0eb9af9afba8e6dab6
Zheng, Lei <realthunder.dev@gmail.com>
2015-10-31 13:49:39 +0800

    Build normal and PIE busybox at the same time

--- build-droid/build-busybox.sh
+++ build-droid/build-busybox.sh
@@ -10,31 +10,46 @@ PKG_URL=http://busybox.net/downloads
 env_setup $@
 pkg_setup $@
 
-conf=$PKG_DIR/.config
-conf_check=$PKG_DIR/.configured$ANDROID_BUILD_PIE
-if test ! -f $conf_check; then
-    if ! cat $TOPDIR/build-droid/patches/$PKG_NAME/android.config | \
-        sed "s|CONFIG_CROSS_COMPILER_PREFIX_TEMPLATE|$DROIDTOOLS-|" | \
-        sed "s|CONFIG_SYSROOT_TEMPLATE|$SYSROOT|" | \
-        sed "s|CONFIG_EXTRA_CFLAGS_TEMPLATE|$CFLAGS|" | \
-        sed "s|CONFIG_EXTRA_LDFLAGS_TEMPLATE|$LDFLAGS $ANDROID_BUILD_PIE|" | \
-        sed "s|CONFIG_EXTRA_LDLIBS_TEMPLATE||" > $conf_check; 
-    then
-        echo "$PKG_NAME configure failed" 
-        exit 1
+test -d $PKG_DIR-pie || cp -a $PKG_DIR $PKG_DIR-pie
+
+build() {
+    local pie=$1
+    conf=$PKG_DIR$pie/.config
+    conf_check=$PKG_DIR$pie/.configured$pie
+    if test ! -f $conf_check; then
+        if ! cat $TOPDIR/build-droid/patches/$PKG_NAME/android.config | \
+            sed "s|CONFIG_CROSS_COMPILER_PREFIX_TEMPLATE|$DROIDTOOLS-|" | \
+            sed "s|CONFIG_SYSROOT_TEMPLATE|$SYSROOT|" | \
+            sed "s|CONFIG_EXTRA_CFLAGS_TEMPLATE|$CFLAGS|" | \
+            sed "s|CONFIG_EXTRA_LDFLAGS_TEMPLATE|$LDFLAGS $pie|" | \
+            sed "s|CONFIG_EXTRA_LDLIBS_TEMPLATE||" > $conf_check; 
+        then
+            echo "$PKG_NAME configure failed" 
+            exit 1
+        fi
     fi
-fi
-if ! diff -q $conf_check $conf &> /dev/null; then
-    cp $conf_check $conf
-fi
+    if ! diff -q $conf_check $conf &> /dev/null; then
+        cp $conf_check $conf
+    fi
 
-cd $PKG_DIR
+    (
+        cd $PKG_DIR$pie
 
-${TOPDIR}/helper/patch.sh $PKG_NAME -v $PKG_VERSION || exit 1
+        ${TOPDIR}/helper/patch.sh $PKG_NAME -v $PKG_VERSION || exit 1
 
-prefix=${SYSROOT}/usr$ANDROID_BUILD_PIE
-mkdir -p $prefix
+        if test "$pie"; then
+            prefix=${SYSROOT}/pie
+        else
+            prefix=${SYSROOT}
+        fi
+        mkdir -p $prefix
 
-make && \
-make CONFIG_PREFIX==$prefix install
+        make && \
+        make CONFIG_PREFIX=$prefix install
+    )
+}
+
+build 
+build -pie
+
 
