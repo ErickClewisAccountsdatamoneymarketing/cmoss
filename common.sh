5a63c053453dd784cfb66a80d6d4d44cc26e0c3e
Zheng, Lei <realthunder.dev@gmail.com>
2018-07-10 16:07:28 +0800

    Add missing mac headers

--- common.sh
+++ common.sh
@@ -240,7 +240,7 @@ env_setup_mac()
     [ $ARCH = arm64 ] && config_arch=arm
     CONFIG_FLAGS="--build=$host_arch-apple-darwin --host=$config_arch-apple-darwin --prefix=$PREFIX"
 
-    cp -n $DEVELOPER/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include/{crt_externs,bzlib}.h $ROOTDIR/include || true
+    cp -n $TOPDIR/mac/*.h $ROOTDIR/include || true
 }
 
 check_url() {
